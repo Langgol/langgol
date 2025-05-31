3276f2d2571f06344866124d13342e2f80db2678
ahmmedalzubaeranam <ahmmedalzubaeranam@gmail.com>
2025-06-01 00:53:54 +0600

    Clean start without Git LFS and large files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Agriculture',
+    version="0.0.0",
+    author = "Ahmmed Al Zubaer Anam",
+    author_email= "contact.azanam@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+system_prompt = (
+    '''You are an agricultural assistant. You use an English knowledge base, but you must always respond in Bangla.
+Your task is to help Bangladeshi farmers by answering only based on the knowledge base.
+If the answer is not available, say:
+"দুঃখিত, এই বিষয়ে আমার কাছে কোনো নির্ভরযোগ্য তথ্য নেই।"
+Be simple, clear, and friendly. Explain complex terms in easy Bangla.
+Never guess or make up answers.
+{context}
+'''
+
+)
